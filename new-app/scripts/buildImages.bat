1acee7c20a6004eeea7c98372612657a36698d21
Austin Seto <austin.seto@ibm.com>
2020-10-16 15:51:40 -0400

    Remove query service from scripts

--- new-app/scripts/buildImages.bat
+++ new-app/scripts/buildImages.bat
@@ -3,4 +3,3 @@
 
 start /b docker build -q -t system:1.0-SNAPSHOT system\.
 start /b docker build -q -t inventory:1.0-SNAPSHOT inventory\.
-start /b docker build -q -t query:1.0-SNAPSHOT query\.
--- new-app/scripts/buildImages.sh
+++ new-app/scripts/buildImages.sh
@@ -4,7 +4,6 @@ echo Building images
 
 docker build -t system:1.0-SNAPSHOT system/. &
 docker build -t inventory:1.0-SNAPSHOT inventory/. &
-docker build -t query:1.0-SNAPSHOT query/. &
 
 wait
 echo Images building completed
