6720396e9b1a7760cc94611548bfed731e53ae5c
Austin Seto <austin.seto@ibm.com>
2021-04-15 15:22:32 -0400

    Add endpoint to set note using HTTP POST request

---
+++ finish/src/main/java/io/openliberty/guides/graphql/SetNote.java
@@ -0,0 +1,36 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.guides.graphql;
+
+import javax.ws.rs.core.Response;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("/setNote")
+public class SetNote {
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProperties(String note) {
+        System.setProperty("note", note);
+        return Response.ok().build();
+    }
+
+}
