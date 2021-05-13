866eb06a05188ef0a0cfd56482fb0b8902c53bd6
Austin Seto <austin.seto@ibm.com>
2021-05-13 16:44:02 -0400

    Acrolinx and typo fixes

--- finish/system/src/main/java/io/openliberty/guides/system/SystemResource.java
+++ finish/system/src/main/java/io/openliberty/guides/system/SystemResource.java
@@ -65,7 +65,7 @@ public class SystemResource {
     }
 
     @GET
-    @Path("name")
+    @Path("names")
     @Produces(MediaType.APPLICATION_JSON)
     public Set<String> propertyList() {
         return System.getProperties().stringPropertyNames();
