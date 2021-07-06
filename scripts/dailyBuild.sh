974415d1474f30366940628ff3d049155e4f595b
Austin Seto <austin.seto@ibm.com>
2021-07-06 15:05:47 -0400

    Remove inventory service, update SEO

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -9,7 +9,7 @@ do
     esac
 done
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" pom.xml
-cat pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml graphql/pom.xml models/pom.xml
+cat system/pom.xml graphql/pom.xml models/pom.xml
 
 ../scripts/testApp.sh
